4322d34388c063e98f4d464374e1857afe20ba9c
Andor <andor.kristen@web.de>
2025-01-06 00:40:55 +0100

    Basic image processing now working. Tested rawpy postprocessing parameters and added cv2 fast nl denoising

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import rawpy
 import argparse
 import os
+import cv2
 
 
 # Setup argument parser
@@ -11,6 +12,9 @@ parser.add_argument("source",
 
 parser.add_argument("-t", "--target",
                     help="Filename or folder where results should be stored. Default is same location as input")
+
+parser.add_argument("--no_denoise", action = "store_true",
+                    help="Wether or not to apply NL means denoising. Default is True. Adding this parameter disables the denoising")
 
 def has_valid_fileending(path: str, target_ending = ".nef") -> bool:
     ending = path[-4:]
@@ -30,6 +34,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
     source: str = os.path.normpath(args.source)
     target: str = args.target
+    denoise: bool = args.no_denoise
 
     # Input parameter checking
     if not os.path.exists(source):
@@ -70,8 +75,24 @@ if __name__ == "__main__":
     # input_filepaths contains all files that need to be processed
     # output_filepaths has a valid output path with valid ending for each entry in input_filepaths
 
-
+    # Main transform loop
     for i in range(len(input_filepaths)):
-        inpt = input_filepaths[i]
-        out = output_filepaths[i]
-        print(f"{inpt} \t--\t {out}")
+        in_path = input_filepaths[i]
+        out_path = output_filepaths[i]
+
+        with rawpy.imread(in_path) as rawpy_image:
+
+            raw_image = rawpy_image.postprocess(output_bps=8, fbdd_noise_reduction = rawpy.FBDDNoiseReductionMode(1), use_auto_wb=True,
+                                                auto_bright_thr=0.0001, highlight_mode = rawpy.HighlightMode.Clip) # Test highlight modes
+            print(raw_image.shape)
+
+            out_image = cv2.cvtColor(raw_image, cv2.COLOR_RGB2BGR)
+            if denoise:
+                out_image = cv2.fastNlMeansDenoisingColored(out_image)
+            cv2.imwrite(out_path, out_image)
+
+    # Delete later
+    # for i in range(len(input_filepaths)):
+    #     inpt = input_filepaths[i]
+    #     out = output_filepaths[i]
+    #     print(f"{inpt} \t--\t {out}")
